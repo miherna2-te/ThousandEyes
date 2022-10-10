281c07914281f910a22e64f791c0c5a919a6c3cd
miherna2-te <112593044+miherna2-te@users.noreply.github.com>
2022-10-10 13:46:45 -0500

    Adding the CSV file
    Adding the CSV file

--- API/AGENT_TO_TEST/agent_to_tests_list.py
+++ API/AGENT_TO_TEST/agent_to_tests_list.py
@@ -4,7 +4,6 @@ from rich.console import Console
 from rich.table import Table
 from getpass import getpass
 
-
 table = Table(title="Agents - Tests List")
 table.add_column("TEST NAME", justify="left", style="cyan", no_wrap=True)
 table.add_column("TEST ID", style="magenta")
@@ -24,24 +23,32 @@ TESTS_API = requests.get(
 ALL_TESTS_CONFIGURED = TESTS_API.json()
 ALL_TESTS_CONFIGURED = ALL_TESTS_CONFIGURED.get("test")
 
-for test in ALL_TESTS_CONFIGURED:
-    test_name = test.get("testName")
-    test_id = str(test.get("testId"))
-    tests_details = requests.get(
-        f"https://api.thousandeyes.com/v6/tests/{test_id}.json",
-        headers=headers,
-        auth=(username, api_key),
-    ).json()
-    test_type = tests_details.get("test")[0].get("type")
-    agents_in_test = tests_details.get("test")[0].get("agents", None)
+with open('tests_to_agents.csv', mode='w') as csv_file:
+    fieldnames = ['TESTNAME', 'TESTID', 'TESTTYPE', 'AGENTS']
+    writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
+    writer.writeheader()
+    
+    for test in ALL_TESTS_CONFIGURED:
+        test_name = test.get("testName")
+        test_id = str(test.get("testId"))
+        tests_details = requests.get(
+            f"https://api.thousandeyes.com/v6/tests/{test_id}.json",
+            headers=headers,
+            auth=(username, api_key),
+        ).json()
+        test_type = tests_details.get("test")[0].get("type")
+        agents_in_test = tests_details.get("test")[0].get("agents", None)
 
-    agents_list = ""
-    if agents_in_test != None:
-        for agent in agents_in_test:
-            agent_name = agent.get("agentName")
-            agents_list += f"{agent_name}\n"
-    table.add_row(test_name, test_id, test_type, agents_list)
+        agents_list = ""
+        agents_csv = ""
+        if agents_in_test != None:
+            for agent in agents_in_test:
+                agent_name = agent.get("agentName")
+                agents_list += f"{agent_name}\n"
+                agents_csv += agent_name + " - "
+        writer.writerow({'TESTNAME': test_name , 'TESTID': test_id, 'TESTTYPE': test_type, 'AGENTS': agents_csv})
+        table.add_row(test_name, test_id, test_type, agents_list)
 
 
-console = Console()
-console.print(table)
+    console = Console()
+    console.print(table)
