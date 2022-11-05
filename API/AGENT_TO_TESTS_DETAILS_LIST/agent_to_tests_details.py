323df79ca23f92671dd818b8b72a72838bbe2d12
miherna2-te <112593044+miherna2-te@users.noreply.github.com>
2022-11-05 00:21:29 -0600

    Update script removing unnecessary lines
    Update script removing unnecessary lines

--- API/AGENT_TO_TESTS_DETAILS_LIST/agent_to_tests_details.py
+++ API/AGENT_TO_TESTS_DETAILS_LIST/agent_to_tests_details.py
@@ -1,28 +1,30 @@
-import requests
 import csv
+import requests
 from getpass import getpass
 
 USERNAME = input("Provide Username: ")
 BASIC_TOKEN = getpass("Provide Basic Token: ")
-AID = input("Provide AID filter: ")
+AID_FILTER = input("Provide AID filter: ")
 
+def api_get_call(USER, TOKEN, URL, AID = None):
 
-def get_tests_information(USERNAME, BASIC_TOKEN, AID):
-    headers = {"content-type": "application/json"}
+    if len(AID) != 0:
+        URL += f"?aid={AID}"
 
-    if AID:
-        url = f"https://api.thousandeyes.com/v6/tests.json?aid={AID}"
-    else:
-        url = "https://api.thousandeyes.com/v6/tests.json"
-
-    tests_api = requests.get(
-        url,
-        headers=headers,
-        auth=(USERNAME, BASIC_TOKEN),
+    response = requests.get(
+        URL,
+        headers = {"content-type": "application/json"},
+        auth=(USER, TOKEN),
     )
 
-    all_tests_configured = tests_api.json()
-    all_tests_configured = all_tests_configured.get("test")
+    result = response.json()
+    result = result.get("test")
+    return result
+
+
+def main(USER, TOKEN, AID):
+    tests_url = "https://api.thousandeyes.com/v6/tests.json"
+    all_tests_configured = api_get_call(USER, TOKEN, tests_url, AID)
 
     with open("tests_to_agents_details.csv", mode="w") as csv_file:
         fields = [
@@ -36,7 +38,7 @@ def get_tests_information(USERNAME, BASIC_TOKEN, AID):
             "url",
             "enabled",
             "alerts",
-            "agents_name",
+            "agent_name",
             "agent_type",
             "shared",
         ]
@@ -54,96 +56,37 @@ def get_tests_information(USERNAME, BASIC_TOKEN, AID):
             url = test.get("url")
             enabled = test.get("enabled")
             alerts = test.get("alertsEnabled")
-            if AID:
-                url_details = (
-                    f"https://api.thousandeyes.com/v6/tests/{test_id}.json?aid={AID}"
-                )
-            else:
-                url_details = f"https://api.thousandeyes.com/v6/tests/{test_id}.json"
-            tests_details = requests.get(
-                url_details,
-                headers=headers,
-                auth=(USERNAME, BASIC_TOKEN),
-            ).json()
-
-            agents_in_test = tests_details.get("test")
-            agents_in_test = agents_in_test[0].get("agents")
-            shared_accounts = tests_details.get("test")
-            shared_accounts = shared_accounts[0].get("sharedWithAccounts")
+            details_url = f"https://api.thousandeyes.com/v6/tests/{test_id}.json"
+            test_details = api_get_call(USER, TOKEN, details_url, AID)
+            agents_in_test = test_details[0].get("agents")
+            shared_accounts = test_details[0].get("sharedWithAccounts")
             shared_accounts = [account.get("name") for account in shared_accounts]
             shared_accounts = " - ".join(shared_accounts)
-
+            row =  {
+                    "test_name": test_name,
+                    "test_id": test_id,
+                    "created": created_date,
+                    "modified": modified_date,
+                    "modified_by": modified_by,
+                    "test_type": test_type,
+                    "protocol": protocol,
+                    "url": url,
+                    "enabled": enabled,
+                    "alerts": alerts,
+                    "agent_name": "",
+                    "agent_type": "",
+                    "shared": shared_accounts,
+                    }
             if agents_in_test is not None:
                 for agent in agents_in_test:
-                    agent_name = agent.get("agentName")
-                    agent_type = agent.get("agentType")
-                    if agent_name:
-                        writer.writerow(
-                            {
-                                "test_name": test_name,
-                                "test_id": test_id,
-                                "created": created_date,
-                                "modified": modified_date,
-                                "modified_by": modified_by,
-                                "test_type": test_type,
-                                "protocol": protocol,
-                                "url": url,
-                                "enabled": enabled,
-                                "alerts": alerts,
-                                "agents_name": agent_name,
-                                "agent_type": agent_type,
-                                "shared": shared_accounts,
-                            }
-                        )
-                        print(
-                            test_name,
-                            test_id,
-                            created_date,
-                            modified_date,
-                            modified_by,
-                            test_type,
-                            protocol,
-                            url,
-                            enabled,
-                            alerts,
-                            agent_name,
-                            agent_type,
-                            shared_accounts,
-                        )
+                    row["agent_name"] = agent.get("agentName")
+                    row["agent_type"] = agent.get("agentType")
+                    writer.writerow(row)
+                    print(row)
             else:
-                writer.writerow(
-                    {
-                        "test_name": test_name,
-                        "test_id": test_id,
-                        "created": created_date,
-                        "modified": modified_date,
-                        "modified_by": modified_by,
-                        "test_type": test_type,
-                        "protocol": protocol,
-                        "url": url,
-                        "enabled": enabled,
-                        "alerts": alerts,
-                        "agents_name": "",
-                        "agent_type": "",
-                        "shared": "",
-                    }
-                )
-                print(
-                    test_name,
-                    test_id,
-                    created_date,
-                    modified_date,
-                    modified_by,
-                    test_type,
-                    protocol,
-                    url,
-                    enabled,
-                    alerts,
-                    " ",
-                    " ",
-                    " ",
-                )
+                writer.writerow(row)
+                print(row)
 
 
 if __name__ == "__main__":
-    get_tests_information(USERNAME, BASIC_TOKEN, AID)
+    main(USERNAME, BASIC_TOKEN, AID_FILTER)
